7aaee5707dc4c776cf0aec537737f188b0474aeb
Serhii <serhiy.ilchenko@gmail.com>
2017-03-05 22:58:03 +0200

    home_20170305_2255

--- js/common.js
+++ js/common.js
@@ -761,4 +761,40 @@ $(document).ready(function() {
 			}
 		});
 	}
+
+	/*subjects slider*/
+	var $subjectSlider = $('.subject-slider-js');
+	if ($subjectSlider.length > 0) {
+
+		$subjectSlider.each(function (){
+			var $currentSlider = $(this);
+
+			$currentSlider.owlCarousel({
+				items: 1,
+				slideBy: 1,
+				dots: false,
+				lazyLoad: false,
+				loop: 1,
+				// onInitialized: functionName,
+				// onResized: functionName,
+				// onTranslated: functionName,
+				stageElement: 'div',
+				navContainer: $currentSlider,
+				navText: ['<span>Предыдущий слайд</span><i></i>', '<span>Следующий слайд</span><i></i>'],
+				nav: true
+				// navText: [
+				// 	'<svg class="owl-svg" enable-background="new 0 0 32 32" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M11.262,16.714l9.002,8.999  c0.395,0.394,1.035,0.394,1.431,0c0.395-0.394,0.395-1.034,0-1.428L13.407,16l8.287-8.285c0.395-0.394,0.395-1.034,0-1.429  c-0.395-0.394-1.036-0.394-1.431,0l-9.002,8.999C10.872,15.675,10.872,16.325,11.262,16.714z" fill="#000" fill-rule="evenodd"/></svg>',
+				// 	'<svg class="owl-svg" enable-background="new 0 0 32 32" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M21.698,15.286l-9.002-8.999  c-0.395-0.394-1.035-0.394-1.431,0c-0.395,0.394-0.395,1.034,0,1.428L19.553,16l-8.287,8.285c-0.395,0.394-0.395,1.034,0,1.429  c0.395,0.394,1.036,0.394,1.431,0l9.002-8.999C22.088,16.325,22.088,15.675,21.698,15.286z" fill="#000" fill-rule="evenodd"/></svg>'
+				// ],
+				// responsive: {
+				// 	0: {
+				// 		items: 1,
+				// 		slideBy: 1
+				// 	}
+				// }
+			});
+		})
+
+	}
+
 });
